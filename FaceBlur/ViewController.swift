05a13be7ee76f9a5ab7cb130b2537833c3707269
bebop-cow <tyrinfern@yahoo.com>
2023-05-15 22:49:42 -0400

    changed method calculateImageViewFrame

--- FaceBlur/ViewController.swift
+++ FaceBlur/ViewController.swift
@@ -10,56 +10,61 @@ import AVFoundation
 import CoreImage
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-
+    
     var captureSession = AVCaptureSession()
     var previewLayer: AVCaptureVideoPreviewLayer!
     var blurEnabled = false
     var blurImageViews = [UIImageView]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCamera()
     }
-
+    
     func setupCamera() {
         let captureDevice = AVCaptureDevice.default(for: .video)
         guard let input = try? AVCaptureDeviceInput(device: captureDevice!), captureSession.canAddInput(input) else { return }
         captureSession.addInput(input)
-
+        
         let output = AVCaptureVideoDataOutput()
         output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(output)
-
+        
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer)
-
+        
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             self?.captureSession.startRunning()
         }
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        previewLayer.frame = view.bounds
+        if let previewLayer = previewLayer {
+            previewLayer.frame = view.bounds
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        }
     }
-
+    
+    
+    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         let image = CIImage(cvPixelBuffer: pixelBuffer)
-
+        
         let options: [String: Any] = [CIDetectorAccuracy: CIDetectorAccuracyHigh,
-                                      CIDetectorMaxFeatureCount: 10]
-
+                               CIDetectorMaxFeatureCount: 10]
+        
         let detector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)
-
+        
         let faces = detector?.features(in: image)
-
+        
         DispatchQueue.main.async {
             for imageView in self.blurImageViews {
                 imageView.removeFromSuperview()
             }
-
+            
             if self.blurEnabled {
                 let blurEffect = UIBlurEffect(style: .regular)
                 for face in faces as? [CIFaceFeature] ?? [] {
@@ -83,53 +88,47 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         var transform = CGAffineTransform.identity
         transform = CGAffineTransform(scaleX: videoBox.width / scaleX, y: videoBox.height / scaleY)
-        transform = transform.translatedBy(x: videoBox.xOffset, y: videoBox.yOffset)
+        transform = transform.translatedBy(x: videoBox.origin.x, y: videoBox.origin.y)
         transform = transform.scaledBy(x: videoBox.width, y: videoBox.height)
         
         let transformedBounds = faceBounds.applying(transform)
         return transformedBounds
     }
+
     
     func videoPreviewBox(for gravity: AVLayerVideoGravity, frameSize: CGSize, apertureSize: CGSize) -> CGRect {
+        var videoBox = CGRect.zero
         let apertureRatio = apertureSize.height / apertureSize.width
         let viewRatio = frameSize.width / frameSize.height
-
-        var size = CGSize.zero
-
-        if gravity == .resizeAspectFill {
+        
+        switch gravity {
+        case .resize:
+            videoBox.size.width = frameSize.width
+            videoBox.size.height = frameSize.height
+        case .resizeAspect:
             if viewRatio > apertureRatio {
-                size.width = frameSize.width
-                size.height = apertureSize.width * (frameSize.width / apertureSize.height)
+                videoBox.size.width = frameSize.height * apertureRatio
+                videoBox.size.height = frameSize.height
+                videoBox.origin.x = (frameSize.width - videoBox.size.width) / 2
             } else {
-                size.width = apertureSize.height * (frameSize.height / apertureSize.width)
-                size.height = frameSize.height
+                videoBox.size.width = frameSize.width
+                videoBox.size.height = frameSize.width / apertureRatio
+                videoBox.origin.y = (frameSize.height - videoBox.size.height) / 2
             }
-        } else if gravity == .resizeAspect {
+        case .resizeAspectFill:
             if viewRatio > apertureRatio {
-                size.width = apertureSize.height * (frameSize.height / apertureSize.width)
-                size.height = frameSize.height
+                videoBox.size.width = frameSize.width
+                videoBox.size.height = frameSize.width / apertureRatio
+                videoBox.origin.y = (frameSize.height - videoBox.size.height) / 2
             } else {
-                size.width = frameSize.width
-                size.height = apertureSize.width * (frameSize.width / apertureSize.height)
+                videoBox.size.width = frameSize.height * apertureRatio
+                videoBox.size.height = frameSize.height
+                videoBox.origin.x = (frameSize.width - videoBox.size.width) / 2
             }
-        } else if gravity == .resize {
-            size.width = frameSize.width
-            size.height = frameSize.height
+        default:
+            break
         }
-
-        var videoBox = CGRect.zero
-        videoBox.size = size
-        if size.width < frameSize.width {
-            videoBox.origin.x = (frameSize.width - size.width) / 2.0
-        } else {
-            videoBox.origin.x = (size.width - frameSize.width) / 2.0
-        }
-        if size.height < frameSize.height {
-            videoBox.origin.y = (frameSize.height - size.height) / 2.0
-        } else {
-            videoBox.origin.y = (size.height - frameSize.height) / 2.0
-        }
-
+        
         return videoBox
     }
 }
